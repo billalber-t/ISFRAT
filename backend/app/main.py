c81b68ad40e9458c28de46f9ae7944d7eba01b2b
Bill Albert <Bill.Albert@Admins-MacBook-Pro-8.local>
2025-07-14 11:04:50 +0300

    Kicks in frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from app.api import endpoints
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI(
     title="ISFRAT API",
     description="REST API for ISFRAT framework",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(endpoints.router)
